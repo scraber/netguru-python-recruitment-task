5a3ed45ccf55fb72e9fa86f0faebdf4685839c63
Mateusz Kacprzak <mrscraber@gmail.com>
2020-11-24 22:45:44 +0100

    Add rate serializer

--- rate/models.py
+++ rate/models.py
@@ -1,10 +1,11 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
 from cars.models import Car
 
 
 class Rate(models.Model):
     car = models.ForeignKey(Car, on_delete=models.CASCADE)
-    rating = models.IntegerField()
+    rating = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])
 
     def __str__(self) -> str:
         return f"{self.car}: {self.rating} stars"
